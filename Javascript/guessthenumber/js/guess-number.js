c80203ee82eec2590bd595a3f38710fcb08dd52a
wellupweb <wellupweb@gmail.com>
2022-12-15 11:59:27 +0600

    hey

--- Javascript/guessthenumber/js/guess-number.js
+++ Javascript/guessthenumber/js/guess-number.js
@@ -1,6 +1,6 @@
 var currentNumber = generateRandomNumber();
 
-console.log(currentNumber);
+
 
 function generateRandomNumber(){
     var randomNumber = Math.random() * 100; // 12.123423 
@@ -8,4 +8,20 @@ function generateRandomNumber(){
     var exactRandomNumber = Math.floor(randomNumber);
 
     return exactRandomNumber;
+}
+
+document.getElementById("check-number").addEventListener('click',play)
+
+
+function play(){
+    var guessNumber = document.getElementById("number-guess").value ;
+
+    if(currentNumber > guessNumber){
+        console.log("Too low");
+    }else if ( currentNumber < guessNumber){
+        console.log("Too High")
+    }else{
+        console.log("You Won")
+    }
+
 }
