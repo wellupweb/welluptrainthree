e081a21e44baefbe3c0bb6e79d206f473be31eba
wellupweb <wellupweb@gmail.com>
2023-01-12 12:29:13 +0600

    hey

---
+++ learningphp/oop/Calculator.php
@@ -0,0 +1,23 @@
+<?php
+    class Calculator{
+        
+        public function add($num1,$num2){
+            $sum = $num1 + $num2;
+            return $sum;
+        } 
+
+        public function multi(){
+
+        }
+
+        public function subs(){
+
+        }
+
+        public function divide(){
+
+        }
+    }
+
+
+?>
---
+++ learningphp/oop/index.php
@@ -0,0 +1,49 @@
+<?php 
+    include "Calculator.php";
+    $sum = "";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $numberone = $_POST['numberone'];
+        $numbertwo = $_POST['numbertwo'];
+
+        $calculator = new Calculator();
+        $sum = $calculator->add($numberone,$numbertwo);
+    
+
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+
+        <form  action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <div class="mb-3">
+                <label for="numberone" class="form-label">Number One</label>
+                <input type="number" class="form-control" name="numberone"  id="numberone">
+            </div>
+
+            <div class="mb-3">
+                <label for="numbertwo" class="form-label">Number Two</label>
+                <input type="number" class="form-control" name="numbertwo" id="numbertwo">
+            </div>
+         
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+        <h2 class="text-center bg-warning text-white"><?php echo $sum ; ?></h2>
+    </div>    
+
+
+</body>
+</html>
